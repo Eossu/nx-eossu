0f8c50664f392d23f8d9edfd8f08d745723dbc43
Idar Bergli <idar.bergli@protonmail.com>
2020-07-30 20:29:26 +0200

    Vertex color change when dragging

--- libs/flowchart/src/lib/directives/vertex.directive.ts
+++ libs/flowchart/src/lib/directives/vertex.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, Input, HostListener, ElementRef } from '@angular/core';
 import { IVertex, ICordinates } from '../flowchart.interfaces';
 import { SvgService } from '../services/svg.service';
+import { colorLuminance } from '../color.helpers';
 
 @Directive({
   selector: '[eossuFcVertex]',
@@ -10,6 +11,7 @@ export class VertexDirective {
 
   private _dragging = false;
   private _offset: ICordinates = { x: 0.0, y: 0.0 };
+  private _originalColor: string = '';
 
   constructor(
     private _elementRef: ElementRef<SVGSVGElement>,
@@ -26,13 +28,15 @@ export class VertexDirective {
   onMouseDown(event: MouseEvent): void {
     if (!this.vertex.readonly) {
       this.calculateMouseOffset(event);
+      this._originalColor = this.vertex.category.color;
       this._dragging = true;
     }
   }
 
   @HostListener('mouseup', ['$event'])
   onMouseUp($event: MouseEvent): void {
-    this._dragging = false; 
+    this._dragging = false;
+    this.vertex.category.color = this._originalColor;
   }
 
   @HostListener('mouseover', ['$event'])
@@ -44,22 +48,28 @@ export class VertexDirective {
   @HostListener('mouseleave', ['$event'])
   onMouseLeave($event: MouseEvent): void {
     this._dragging = false;
+    if (this._originalColor) this.vertex.category.color = this._originalColor;
   }
 
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent): void {
     if (this._dragging) {
       event.preventDefault();
-      const svgPoint = this._svgDragSvc.getSVGPoint(
+
+      if (this._originalColor)
+        this.vertex.category.color = colorLuminance(this._originalColor, 0.12);
+
+      const coord = this._svgDragSvc.getSVGPoint(
         event,
         this._elementRef.nativeElement
       );
-      let x = svgPoint.x - this._offset.x;
-      let y = svgPoint.y - this._offset.y;
+
+      let x = coord.x - this._offset.x;
+      let y = coord.y - this._offset.y;
 
       if (x < 0) x = 0;
-      if (y < 0) y =0;
-      
+      if (y < 0) y = 0;
+
       this.vertex.x = x;
       this.vertex.y = y;
 
@@ -68,8 +78,11 @@ export class VertexDirective {
   }
 
   private calculateMouseOffset(event: MouseEvent): void {
-    this._offset = this._svgDragSvc.getSVGPoint(event, this._elementRef.nativeElement);
+    this._offset = this._svgDragSvc.getSVGPoint(
+      event,
+      this._elementRef.nativeElement
+    );
     this._offset.x -= this.vertex.x;
     this._offset.y -= this.vertex.y;
-  } 
+  }
 }
