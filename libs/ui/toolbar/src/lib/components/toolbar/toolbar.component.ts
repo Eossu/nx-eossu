3b589e0b8577e685f0557f8dd9fb4d942c599dec
Idar Bergli <idar.bergli@protonmail.com>
2020-08-10 00:25:32 +0200

    Added basic styles to eossu-ui-elements and added toolbar component.

---
+++ libs/ui/toolbar/src/lib/components/toolbar/toolbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'eossu-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/ui/toolbar/src/lib/ui-toolbar.module.ts
+++ libs/ui/toolbar/src/lib/ui-toolbar.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ToolbarComponent],
+  exports: [ToolbarComponent],
 })
 export class UiToolbarModule {}
