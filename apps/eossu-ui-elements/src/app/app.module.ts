e62d230661e10cd484268ba81688c245e4b0a86d
Idar Bergli <idar.bergli@protonmail.com>
2020-08-10 16:37:49 +0200

    Loding towers

--- apps/eossu-ui-elements/src/app/app.module.ts
+++ apps/eossu-ui-elements/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { UiToolbarModule } from '@eossu/ui/toolbar';
+import { UiLoadersModule } from '@eossu/ui/loaders';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiToolbarModule],
+  imports: [BrowserModule, UiToolbarModule, UiLoadersModule],
   providers: [],
   bootstrap: [AppComponent],
 })
